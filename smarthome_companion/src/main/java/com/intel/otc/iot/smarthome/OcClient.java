d73400341a3c7c50c7ff94c94a8d07375bf46383
Tonny Tzeng <tonny.tzeng@gmail.com>
2016-11-02 22:46:43 +0800

    Initial checkin with webview, LED and Color RGB representations

---
+++ smarthome_companion/src/main/java/com/intel/otc/iot/smarthome/OcClient.java
@@ -0,0 +1,77 @@
+package com.intel.otc.iot.smarthome;
+
+import android.content.Context;
+import android.util.Log;
+
+import org.iotivity.base.ModeType;
+import org.iotivity.base.OcConnectivityType;
+import org.iotivity.base.OcException;
+import org.iotivity.base.OcPlatform;
+import org.iotivity.base.OcResource;
+import org.iotivity.base.OcResourceIdentifier;
+import org.iotivity.base.PlatformConfig;
+import org.iotivity.base.QualityOfService;
+import org.iotivity.base.ServiceType;
+
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class OcClient implements OcPlatform.OnResourceFoundListener {
+    private static final String TAG = OcClient.class.getSimpleName();
+
+    public interface OnResourceFound {
+        void onResourceFound(OcResource resource);
+    }
+    private OnResourceFound onResourceFoundListener;
+    private Map<OcResourceIdentifier, OcResource> mResourceFound = new HashMap<>();
+
+    public OcClient(Context context, OnResourceFound listener) {
+        onResourceFoundListener = listener;
+        PlatformConfig platformConfig = new PlatformConfig(
+                context,
+                ServiceType.IN_PROC,
+                ModeType.CLIENT,
+                "0.0.0.0", // By setting to "0.0.0.0", it binds to all available interfaces
+                0,         // Uses randomly available port
+                QualityOfService.LOW
+        );
+        OcPlatform.Configure(platformConfig);
+    }
+
+    public synchronized void findResources(final String ocResourceType) {
+        mResourceFound.clear();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    OcPlatform.OnResourceFoundListener listener = OcClient.this;
+                    String requestUri = OcPlatform.WELL_KNOWN_QUERY + "?rt=" + ocResourceType;
+                    OcPlatform.findResource("", requestUri, EnumSet.of(OcConnectivityType.CT_DEFAULT), listener);
+                    TimeUnit.SECONDS.sleep(1);
+                    /* Find resource is done twice so that we discover the original resources a second time.
+                     * These resources will have the same uniqueidentifier (yet be different objects),
+                     * so that we can verify/show the duplicate-checking code in foundResource();
+                     */
+                    OcPlatform.findResource("", requestUri, EnumSet.of(OcConnectivityType.CT_DEFAULT), listener);
+                } catch (OcException | InterruptedException e) {
+                    Log.e(TAG, e.toString());
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    public void onResourceFound(OcResource ocResource) {
+        if (!mResourceFound.containsKey(ocResource.getUniqueIdentifier())) {
+            Log.d(TAG, "Found resource " + ocResource.getHost() + ocResource.getUri() +
+                        " for the first time on server with ID " + ocResource.getServerId());
+            mResourceFound.put(ocResource.getUniqueIdentifier(), ocResource);
+        } else {
+            Log.d(TAG, "Found resource " + ocResource.getHost() + ocResource.getUri());
+            if (null != onResourceFoundListener)
+                onResourceFoundListener.onResourceFound(ocResource);
+        }
+    }
+}
---
+++ smarthome_companion/src/main/java/com/intel/otc/iot/smarthome/ResourceAdapter.java
@@ -0,0 +1,90 @@
+package com.intel.otc.iot.smarthome;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.util.Pair;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.iotivity.base.OcException;
+import org.iotivity.base.OcResource;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResourceAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private static final String TAG = ResourceAdapter.class.getSimpleName();
+    private enum CardTypes {
+        SwitchBinary,
+        ColorRGB,
+    }
+
+    private Context mContext;
+    private Map<String, CardTypes> mSupportedResourceType = new HashMap<>();
+    private ArrayList<Pair<CardTypes, OcResource>> mCardList = new ArrayList<>();
+
+    public ResourceAdapter(Context context) {
+        mContext = context;
+        mSupportedResourceType.put(CardSwitchBinary.RESOURCE_TYPE, CardTypes.SwitchBinary);
+        mSupportedResourceType.put(CardColorRGB.RESOURCE_TYPE, CardTypes.ColorRGB);
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        Log.d(TAG, "getItemViewType(" + position + ")");
+        return mCardList.get(position).first.ordinal();
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Log.d(TAG, "onCreateViewHolder(" + viewType + ")");
+        RecyclerView.ViewHolder card = null;
+        if (viewType == CardTypes.SwitchBinary.ordinal()) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_onoff, parent, false);
+            card = new CardSwitchBinary(v, mContext);
+        } else if (viewType == CardTypes.ColorRGB.ordinal()) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_colorpicker, parent, false);
+            card = new CardColorRGB(v, mContext);
+        }
+        return card;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        Log.d(TAG, "onBindViewHolder(" + position + ")");
+        ((CardOcResource) holder).bindResource(mCardList.get(position).second);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCardList.size();
+    }
+
+    public void add(final OcResource resource) {
+        for (String rt : resource.getResourceTypes())
+            if (mSupportedResourceType.containsKey(rt)) {
+                mCardList.add(new Pair<CardTypes, OcResource>(mSupportedResourceType.get(rt), resource));
+            }
+        ((Activity) mContext).runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                notifyDataSetChanged();
+            }
+        });
+    }
+
+    public void clear() {
+        try {
+            for (Pair<CardTypes, OcResource> card : mCardList)
+                card.second.cancelObserve();
+        } catch (OcException e) {
+            Log.e(TAG, e.toString());
+        }
+        mCardList.clear();
+        notifyDataSetChanged();
+    }
+}
